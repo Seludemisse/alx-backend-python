06613920e726c317d593bf3f53fa8ee1d6eaad6c
Seludemisse <seludemisse12@gmail.com>
2025-07-13 13:26:37 -0700

    Adding 1-batch_processing.py 4-stream_ages.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,18 +1,4 @@
-import seed
-
-def stream_users_in_batches(batch_size):
-    connection = seed.connect_to_prodev()
-    cursor = connection.cursor(dictionary=True)
-    cursor.execute("SELECT COUNT(*) FROM user_data")
-    total = cursor.fetchone()['COUNT(*)']
-    
-    for offset in range(0, total, batch_size):
-        cursor.execute("SELECT * FROM user_data LIMIT %s OFFSET %s", (batch_size, offset))
-        yield cursor.fetchall()
-    
-    cursor.close()
-    connection.close()
-
-def batch_process(data, batch_size):
-    for i in range(0, len(data), batch_size):
-        yield data[i:i + batch_size]
+def batch_processing(batch):
+    for user in batch:
+        if user.get('age') > 25:
+            yield user
--- python-generators-0x00/4-stream_ages.py
+++ python-generators-0x00/4-stream_ages.py
@@ -1,12 +1,12 @@
-def stream_user_ages(users):
-    for user in users:
-        yield user['age']  
+users_data = []  
 
- 
-def calculate_average_age(users):
+def stream_user_ages():
+    for user in users_data:
+        yield user['age']
+def calculate_average_age():
     total_age = 0
     count = 0
-    for age in stream_user_ages(users):
+    for age in stream_user_ages():
         total_age += age
         count += 1
     return total_age / count if count > 0 else 0
