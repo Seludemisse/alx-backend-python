fbe0c54cec970981cb343921c87cdbb1c0d95e18
Seludemisse <seludemisse12@gmail.com>
2025-07-13 13:32:06 -0700

    Fixing issues on batch_processing

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,3 +1,18 @@
+import seed
+
+def stream_users_in_batches(batch_size):
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT COUNT(*) FROM user_data")
+    total = cursor.fetchone()['COUNT(*)']
+
+    for offset in range(0, total, batch_size):
+        cursor.execute("SELECT * FROM user_data LIMIT %s OFFSET %s", (batch_size, offset))
+        yield cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
 def batch_processing(batch):
     for user in batch:
         if user.get('age') > 25:
