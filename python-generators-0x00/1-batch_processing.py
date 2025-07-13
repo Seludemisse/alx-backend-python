0ac33f242af2738e8a8a5264dd62401cddd184f3
Seludemisse <seludemisse12@gmail.com>
2025-07-13 11:40:12 -0700

    Adding new files

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,20 @@
+import seed
+
+def stream_users_in_batches(batch_size):
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT COUNT(*) FROM user_data")
+    total = cursor.fetchone()['COUNT(*)']
+    
+    for offset in range(0, total, batch_size):
+        cursor.execute("SELECT * FROM user_data LIMIT %s OFFSET %s", (batch_size, offset))
+        yield cursor.fetchall()
+    
+    cursor.close()
+    connection.close()
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user['age'] > 25:
+                print(user)
