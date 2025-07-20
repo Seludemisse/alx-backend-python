0014f08b8d7d5bc0902da9a3f1b69c4126dee615
Selu <seludemisse12@gmail.com>
2025-07-19 21:35:09 -0700

    Add unit tests for utils.access_nested_map with parameterized tests

--- python-generators-0x00/0x03-Unittests_and_integration_tests/test_utils.py
+++ python-generators-0x00/0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,8 @@ Unit tests for utils.access_nested_map
 """
 import unittest
 from parameterized import parameterized
-from utils import  access_nested_map
+from utils import access_nested_map
+
 
 class TestAccessNestedMap(unittest.TestCase):
     """Tests for access_nested_map"""
@@ -17,15 +18,16 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test access_nested_map returns expected result"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
     @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map_exception(self, nested_map, path):
-        """Test access_nested_map raises KeyError with invalid path"""
-        with self.assertRaises(KeyError) as context:
+        """Test access_nested_map raises KeyError """
+        with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), f"'{path[-1]}'")
+
 
 if __name__ == "__main__":
     unittest.main()
